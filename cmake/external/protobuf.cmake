cffd50e8f32a23c93c4f7de124d398cf4f22b3ee
Henrik Schnor <hschnor@mailbox.org>
2024-05-26 13:20:39 +0200

    Build with C++17
    The abseil dependency, which is pulled in by protobuf, seems to
    assume that C++17 is being used, so the build fails with C++14.

--- cmake/external/protobuf.cmake
+++ cmake/external/protobuf.cmake
@@ -153,6 +153,7 @@ include (ExternalProject)
 ExternalProject_Add(${PROTOBUF_TARGET}
     PREFIX ${PROTOBUF_TARGET}
     GIT_REPOSITORY https://github.com/google/protobuf.git
+    GIT_SHALLOW TRUE
     GIT_TAG v25.2
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_COMMAND} ${PROTOBUF_INSTALL_DIR}/src/${PROTOBUF_TARGET}
